f780d413ad5f9ec52ca0c5a6fd1b25f770c58493
Kurt Brown <k.brown1990@yahoo.com>
2020-05-05 16:57:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   //var string = "i can't hear you!"
-  if(string.toLowerCase()) === string
+  if(string.toLowerCase()) == string
   return "I can't hear you!"
 }
